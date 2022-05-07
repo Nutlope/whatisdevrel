bc99950eb7aea5a6fdee2318377331089b28df56
nutlope <he55@drexel.edu>
2022-05-07 16:34:29 -0400

    mobile responsiveness

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Essentially use the content from my devrel blog post and devrel repo.
 - [x] Create basic homepage
 - [x] Add all the content
 - [x] Remove down arrow and replace with a nice button on the homepage
-- [ ] Fix mobile responsiveness issue of homepage
+- [x] Fix mobile responsiveness issue of homepage
 - [ ] Fix the picture that I have by asking a designer to redo it
 - [ ] Get smooth scrolling to work with a ref
 - [ ] Finish v1 and share with Lee, Shawn (& sam / tejas?)
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="relative px-4 pb-10 text-center sm:px-6 md:grid md:min-h-screen md:place-items-center">
+    <header className="relative px-4 pb-10 pt-20 text-center md:grid md:min-h-screen md:place-items-center md:px-6 md:pt-0">
       <div className="mx-auto max-w-max">
         <div className="w-full">
           <span className="text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-8xl">
@@ -10,8 +10,8 @@ export default function Header() {
             .com
           </span>
         </div>
-        <div className="mt-4 mb-8">
-          <h1 className="py-3 px-8 text-2xl font-normal italic text-gray-800 dark:text-gray-300">
+        <div className="mt-2 mb-6">
+          <h1 className="py-3 px-8 text-xl font-normal italic text-gray-800 dark:text-gray-300 md:text-2xl">
             All you need to know about{" "}
             <span className="text-blue-500">DevRel</span> and how to get into
             it.
@@ -21,7 +21,7 @@ export default function Header() {
           title="Go to DevRel at a glance"
           href="#devrel-at-a-glance"
           aria-hidden="true"
-          className="flex w-full items-center justify-center scroll-smooth rounded-md border border-transparent bg-blue-500 px-8 py-3 font-medium text-white hover:bg-blue-700 md:py-4 md:px-10 md:text-lg"
+          className="flex items-center justify-center scroll-smooth rounded-md border border-transparent bg-blue-500 px-8 py-3 font-medium text-white hover:bg-blue-700 md:w-full md:py-4 md:px-10 md:text-lg"
         >
           {" "}
           Learn more
