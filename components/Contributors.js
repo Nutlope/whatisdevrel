8ac4c4d1b807f4ae5a26aba320ac82f18fc826e4
TJ Kohli <tj@monogram.io>
2022-09-14 18:57:26 -0400

    More Contributors work

--- components/Contributors.js
+++ components/Contributors.js
@@ -42,64 +42,60 @@ export default function Contributors() {
       >
         {contributors.map((person) => (
           <li key={person.name}>
-            <div className="space-y-6">
-              <Image
-                className="aspect-square w-48 rounded-full"
-                src={person.imageUrl}
-                alt="picture of a contributor"
-                width="180"
-                height="180"
-              />
-              <div className="">
-                <div className="space-y-1 text-lg font-medium leading-6">
-                  <p className="font-medium">{person.name}</p>
-                  <div>{person.role}</div>
-                </div>
-                <ul className="mt-3 flex justify-center">
-                  {/* Twitter */}
-                  <a
-                    href={person.twitterUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="link p-3 no-underline"
-                  >
-                    <span className="sr-only">Twitter</span>
-                    <svg
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                  </a>
+            <Image
+              className="mb-6 aspect-square w-48 rounded-full"
+              src={person.imageUrl}
+              alt="picture of a contributor"
+              width="180"
+              height="180"
+            />
+            <h3 className="title font-mediumtext-lg mb-1 font-sans font-medium leading-6">
+              {person.name}
+            </h3>
+            <p className="text-base">{person.role}</p>
+            <ul className="mt-3 flex justify-center">
+              {/* Twitter */}
+              <a
+                href={person.twitterUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link rounded-md p-3 hover:bg-gray-500/10"
+              >
+                <span className="sr-only">Twitter</span>
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+              </a>
 
-                  {/* Website */}
-                  <a
-                    href={person.websiteUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="link p-3 no-underline"
-                  >
-                    <span className="sr-only">Website</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                      />
-                    </svg>
-                  </a>
-                </ul>
-              </div>
-            </div>
+              {/* Website */}
+              <a
+                href={person.websiteUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link rounded-md p-3 hover:bg-gray-500/10"
+              >
+                <span className="sr-only">Website</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                  />
+                </svg>
+              </a>
+            </ul>
           </li>
         ))}
       </ul>
